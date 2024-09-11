c8c189bb7138a19a20251946036f186ba43c500b
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-09-11 18:44:10 +0530

    Update min_max.java

--- min_max.java
+++ min_max.java
@@ -1,1 +1,23 @@
 
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        Collections.sort(a);
+
+        long mindiff = Long.MAX_VALUE;
+
+        for (int i = 0; i <= n - m; i++) {
+            
+            long diff = a.get(i + m - 1) - a.get(i);
+            
+            if (diff < mindiff) {
+                mindiff = diff;
+            }
+        }
+        return minDiff;
+        
+        
+        // your code here
+    }
+}
