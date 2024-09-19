733dae3142737d2b853fc76848c0d01bdc970537
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-09-19 22:44:48 +0530

    Create Morres_Algo.java

---
+++ arrays/Morres_Algo.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 1,12,12,12,12,12,12,2, 2, 2, 2};
+        int freq = 0;
+        int ans = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (freq == 0) {
+                ans = arr[i]; 
+            }
+             if (ans == arr[i]) {
+                freq++; 
+                } else {
+                freq--; 
+            }
+        }
+        
+        System.out.println(ans);
+    }
+}
