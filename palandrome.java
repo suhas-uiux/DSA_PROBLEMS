780cdf06a94ed4f736af2a77ef858d34b8188d44
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-09-21 13:53:15 +0530

    Create palandrome.java

---
+++ palandrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+         if (x < 0) {
+            return false;
+        }
+        int o = x;
+        int r= 0;
+        while (x != 0) {
+            int d= x % 10;
+            r= r* 10 + d;
+            x /= 10;
+        }
+        return o == r;
+        
+    }
+}
