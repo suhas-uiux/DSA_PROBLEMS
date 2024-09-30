f8afd3efc208d8585ff0e255ca8b722126da8af8
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-09-30 23:48:41 +0530

    Update great_in_subarray.java

--- great_in_subarray.java
+++ great_in_subarray.java
@@ -1,3 +1,6 @@
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 class Solution {
     public int maxSubArray(int[] nums) {
      int sum=0;
