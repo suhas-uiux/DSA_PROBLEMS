0b4065ce45f63fa7c7d853665983ce4c35760910
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-09-30 23:45:57 +0530

    Update array_rotate.java

--- array_rotate.java
+++ array_rotate.java
@@ -1,3 +1,9 @@
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
 class Solution {
     public void rotate(int[] nums, int k) {
         int[] arr=new int[nums.length];
