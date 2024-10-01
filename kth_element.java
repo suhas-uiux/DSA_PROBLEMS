8de387f8d5aa8bbde72dcb7ee828a53a4fcc71ad
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-10-01 23:57:04 +0530

    Update kth_element.java

--- kth_element.java
+++ kth_element.java
@@ -1,3 +1,12 @@
+// Example 1:
+
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+// Example 2:
+
+// Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+// Output: 4
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
