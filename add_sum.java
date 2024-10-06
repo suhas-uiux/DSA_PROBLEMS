20223adc28592ad16bee733b8f9bbfd0813eb9cf
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-10-06 23:54:28 +0530

    Update add_sum.java

--- add_sum.java
+++ add_sum.java
@@ -1,3 +1,9 @@
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
 class Solution {
     public int addDigits(int num) {
         if(num==0){
