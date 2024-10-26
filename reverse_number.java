a0a4608e340dea2baf85ba13fbc94a6999acd98d
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-10-26 12:06:45 +0530

    Update reverse_number.java

--- reverse_number.java
+++ reverse_number.java
@@ -13,17 +13,15 @@ class Solution {
         
         while (x != 0) {
             int digit = x % 10;  
-            x /= 10;             
+                       
             
            
-            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
-                return 0;  
-            }
-            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+            if (reversed > Integer.MAX_VALUE / 10 || reversed < Integer.MIN_VALUE / 10) {
                 return 0;  
             }
             
             reversed = reversed * 10 + digit;
+             x /= 10; 
         }
         
         return reversed;
