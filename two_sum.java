ed332cee7dc41b9ff2e4fba16f63ec43023e5e3a
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-10-28 22:41:00 +0530

    Update two_sum.java

--- two_sum.java
+++ two_sum.java
@@ -1,17 +1,23 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] val=new int[2];
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                sum=nums[i]+nums[j];
-                if(sum==target){
-                    val[0]=i;
-                    val[1]=j;
-                    
-                }
-            }
+      Map<Integer,Integer> hm=new HashMap<Integer,Integer>();
+      for(int i=0;i<nums.length;i++){
+        int req_num=target-nums[i];
+        if(hm.containsKey(req_num)){
+            int[] arr={hm.get(req_num),i};
+            return arr;
         }
-        return val;
+        hm.put(nums[i],i);
+      }
+      return null;
     }
 }
