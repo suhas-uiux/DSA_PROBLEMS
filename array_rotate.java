1d20f19f9f8f3dbd7ff8838c7fd5d5fd98bfa474
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-11-29 09:55:10 +0530

    Update array_rotate.java

--- array_rotate.java
+++ array_rotate.java
@@ -17,3 +17,28 @@ class Solution {
             System.arraycopy(arr, 0, nums, 0, nums.length);
     }
 }
+
+
+//code with the 0(1) time complexity
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(k>0){
+            k=k%nums.length;
+        }
+        reverseArray(nums,0,nums.length-1);
+        reverseArray(nums,0,k-1);
+        reverseArray(nums,k,nums.length-1);
+    }
+     void reverseArray(int nums[],int start,int end){
+        while(start<end){
+
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start++;
+        end--;
+        }
+
+    }
+}
