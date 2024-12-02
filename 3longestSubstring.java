c45f8c05693a08a8e790f6657a58f0618175506a
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2024-12-03 00:18:04 +0530

    Create 3longestSubstring.java

---
+++ 3longestSubstring.java
@@ -0,0 +1,41 @@
+// Given a string s, find the length of the longest 
+// substring
+//  without repeating characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set=new HashSet<>();
+        int max=0;
+        int left=0;
+        for(int right=0;right<s.length();right++){
+            char ch=s.charAt(right);
+            while(set.contains(ch)){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(ch);
+            int curr=right-left+1;
+             max=Math.max(max,curr);
+        }       
+        return max;
+    }
+}
