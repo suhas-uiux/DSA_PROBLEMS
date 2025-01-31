176ff791886b9ef1f5a6a43658eaebe171264f51
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2025-01-31 16:32:48 +0530

    Create 20_validParantheses.java

---
+++ 20_validParantheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}') {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else if (stack.peek() == '(' && s.charAt(i) == ')' ||
+                        stack.peek() == '[' && s.charAt(i) == ']' ||
+                        stack.peek() == '{' && s.charAt(i) == '}')
+                {
+                    stack.pop();
+                }
+                else{
+                    return false;
+                } 
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
