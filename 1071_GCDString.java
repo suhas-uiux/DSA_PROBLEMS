7d66850e579730faec54415d051d682f0e645534
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2025-02-04 22:17:17 +0530

    Create 1071_GCDString.java

---
+++ 1071_GCDString.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+        int gcdlen=gcd(str1.length(),str2.length());
+
+        return str1.substring(0,gcdlen);
+        
+    }
+    private int gcd(int a,int b){
+            return b==0 ? a : gcd(b,a%b);
+        }
+}
